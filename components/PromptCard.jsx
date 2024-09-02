080454810ca656c0d6962e6d52e8c6fc3c188695
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-02 16:35:17 -0500

    div tag cleanup

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { usePathName, useRouter } from 'next/navigation';
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handkeDelete }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
   
   return (
@@ -19,30 +19,32 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handkeDelete }) => {
             height={40}
             className="rounded-full object-object-contain"
           />
+        
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col">
-          <h3 className="font-satoshi font-semibold text-gray-900">
-            {post.creator.username}
-          </h3>
-          <p className="font-inter text-sm text-gray-500">
-            {post.creator.email}
-          </p>
+        <div className="copy_btn" onClick={() => {}}>
+          <Image 
+            src={
+              copied === post.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
         </div>
-      </div>
-
-      <div className="copy_btn" onClick={() => {}}>
-        <Image 
-          src={copied === post.prompt
-            ? '/assets/icons/tick.svg'
-            : '/assets/icons/copy.svg'
-          }
-          width={12}
-          height={12}
-        />
       </div>
     </div>
-  )
+  );
 }
 
 export default PromptCard
