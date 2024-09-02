77a200d96263966afbf61b5c30945867e65eb265
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-02 16:45:41 -0500

    Added copy to clipboard functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,12 @@ import { usePathName, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  }
   
   return (
     <div className="prompt_card">
@@ -31,7 +37,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </div>
         </div>
 
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image 
             src={
               copied === post.prompt
@@ -43,6 +49,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
         </div>
       </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
+      onClick={() => handleClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   );
 }
