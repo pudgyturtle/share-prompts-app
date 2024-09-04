dcd518ef0824446d3d134a247e77c7915a241132
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-09-04 10:35:29 -0500

    Added edit functionality, previous commit was only the button

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,8 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+    const router = useRouter(); 
+    
     const { data: session } = useSession();
 
     const [posts, setPosts] = useState([]);
@@ -22,11 +24,11 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts();
       }, []);
   
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
 
     }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -62,7 +62,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
-        <div>
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}
           >
